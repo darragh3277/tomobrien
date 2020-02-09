256afb85b68b6a0b53c4e8e9b3a3975f728784e0
Darragh <darragh3277@gmail.com>
2020-02-09 12:45:49 +0000

    configure recaptcha

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,27 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\SendContactForm;
 use App\Mail\ContactUsReceived;
 use App\Mail\ContactUsSent;
 
 class ContactController extends Controller
 {
 	public function index()
-	{
-		
-		return view('contact');
-	
+	{		
+		return view('contact');	
 	}
 	
-	public function send()
-	{
-		
-		$this->validate(request(), [
-			'name' => 'required',
-			'email' => 'required|email',
-			'message' => 'required'
-		]);
-		
+	/**
+	 * @param SendContactForm $request
+	 */
+	public function send(SendContactForm $request)
+	{		
 		\Mail::to(request('email'))->send(new ContactUsReceived(request()->all()));
 		\Mail::to(config('mail.inbox'))->cc(config('mail.cc'))->send(new ContactUsSent(request()->all()));
 		
